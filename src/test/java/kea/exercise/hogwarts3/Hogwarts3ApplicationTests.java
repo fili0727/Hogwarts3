d58a9ca04ed0df67577f187186dcff1ef45332ad
fili0727 <123998888+fili0727@users.noreply.github.com>
2024-02-14 09:46:56 +0100

    first commit

---
+++ src/test/java/kea/exercise/hogwarts3/Hogwarts3ApplicationTests.java
@@ -0,0 +1,13 @@
+package kea.exercise.hogwarts3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hogwarts3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
